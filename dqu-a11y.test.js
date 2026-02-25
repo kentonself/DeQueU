1ccfe61739767eb820450bbe561f6a332c354b7a
kentonself <kentonself@gmail.com>
2026-02-25 11:26:48 -0600

    Code clean up

--- dqu-a11y.test.js
+++ dqu-a11y.test.js
@@ -2,32 +2,33 @@ const { AxeBuilder } = require('@axe-core/webdriverjs');
 require('chromedriver'); // This line makes the chromedriver executable available
 const { Builder } = require('selenium-webdriver');
 
-async function t() {
-    let driver = await new Builder().forBrowser('chrome').build();
-    let results = {}
-    try {
-        await driver.get('https://dequeuniversity.com/demo/mars');
-        results = await new AxeBuilder(driver).analyze();
-        //console.log(results)
-    } catch (error) {
-        console.error(`Could not find mainNav ${error}`)
-    } finally {
-        await driver.quit();
-    }
-    return results
-}
-
 let a11yCases = [
     { "impact": "moderate", "upperLimit": 15 },
     { "impact": "serious", "upperLimit": 10 },
     { "impact": "critical", "upperLimit": 5 }
 ]
 
-test.each(a11yCases)("Is Catagory over limit", async ({ impact, upperLimit }) => {
-    const results = await t()
+test(`Does the page meet the accessibility requirements?`, async () => {
+    let results = ""
+    failureString = "";
+    let driver = await new Builder().forBrowser('chrome').build();
+    await driver.get('https://dequeuniversity.com/demo/mars');
+    results = await new AxeBuilder(driver).analyze();
+    //console.log(results)
     const violations = results.violations
-    //console.log(violations)
-    console.log(results)
-    impacts = violations.filter(violation => violation.impact == impact)
-    expect(impacts.length).toBeLessThanOrEqual(upperLimit)
+    //console.log(violations) // Show only the violations
+    console.log(results) // Show ALL the results
+    for (const a11yCase of a11yCases) {
+        // console.log(`${a11yCase.impact}: ${a11yCase.upperLimit} : `)
+        let impacts = violations.filter(violation => violation.impact == a11yCase.impact)
+        if (impacts.length > a11yCase.upperLimit) {
+            if(failureString.length > 0) {
+                failureString += "\n"
+            }
+            failureString = `${failureString}Too many ${a11yCase.impact} violations. ` +
+                `Expecing less than ${a11yCase.upperLimit}. Received ${impacts.length}.`
+        }
+    }
+    await expect(failureString).toBe("")
+    await driver.quit();
 })
--- dqu-nav.test.js
+++ dqu-nav.test.js
@@ -3,14 +3,9 @@ const { Builder, By } = require('selenium-webdriver');
 
  test('Ensure Nav Bar is found', async () => {
   let driver = await new Builder().forBrowser('chrome').build();
-  try {
     await driver.get('https://dequeuniversity.com/demo/mars');
     const mainNav = await driver.findElement(By.id('main-nav'))
     await expect(mainNav).not.toBeNull()
-  } catch (error) {
-    console.error(`Could not find mainNav ${error}`)
-  } finally {
     await driver.quit();
-  }
  })
 
